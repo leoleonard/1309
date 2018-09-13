2375decf9a49c0fbbab95a04b016cc5fdef51b68
Damian Juzyk <damian.juzyk@gmail.com>
2018-09-13 20:54:53 +0200

    add 1.1

---
+++ src/Fridge.java
@@ -0,0 +1,14 @@
+public class Fridge {
+    String name;
+    boolean vegetable;
+    boolean fruit;
+    String expiryDate;
+    int weight;
+
+    Fridge (String name, boolean vegetable, boolean fruit, String expiryDate, int weight) {
+        this.name = name;
+        this.vegetable = vegetable;
+        this.expiryDate = expiryDate;
+        this.weight = weight;
+    }
+}
