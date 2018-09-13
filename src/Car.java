f8ac954f5b40da4685c71fef0821f5b63ccc38e6
Damian Juzyk <damian.juzyk@gmail.com>
2018-09-13 21:30:46 +0200

    add 2.1

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    String brand;
+    String model;
+    int year;
+    double capacity;
+
+    Car (String brand, String model, int year, double capacity) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.capacity = capacity;
+    }
+}
