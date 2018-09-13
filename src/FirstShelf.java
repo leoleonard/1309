e7044c59a5410e3ee55fd03da3ca5f9bb1fbb57e
Damian Juzyk <damian.juzyk@gmail.com>
2018-09-13 21:16:58 +0200

    add 1.2 - wersja prawidlowa

--- src/FirstShelf.java
+++ src/FirstShelf.java
@@ -1,24 +1,24 @@
 public class FirstShelf {
     public static void main(String[] args) {
-//        Fridge apple = new Fridge("Apple", false, true, "next week", 100);
-//        Fridge cabbage = new Fridge("Cabbage", true, false, "tomorrow", 400);
-//        Fridge chocolate = new Fridge("Chocolate", false, false, "next year", 100);
-//
-//        System.out.println("Product: "+ apple.name);
-//        System.out.println("Fruit? " + apple.fruit);
-//        System.out.println("Expiry date: " + apple.expiryDate);
-//        System.out.println("Weight: " + apple.weight + 'g');
-//
-//        System.out.println("Product: "+ cabbage.name);
-//        System.out.println("Vegatable? " + cabbage.vegetable);
-//        System.out.println("Expiry date: " + cabbage.expiryDate);
-//        System.out.println("Weight: " + cabbage.weight + 'g');
-//
-//        System.out.println("Product: "+ chocolate.name);
-//        System.out.println("Fruit? " + chocolate.fruit);
-//        System.out.println("Vegatable? " + cabbage.vegetable);
-//        System.out.println("Expiry date: " + chocolate.expiryDate);
-//        System.out.println("Weight: " + chocolate.weight + 'g');
+        Fridge apple = new Fridge("Apple", false, true, "next week", 100);
+        Fridge cabbage = new Fridge("Cabbage", true, false, "tomorrow", 400);
+        Fridge chocolate = new Fridge("Chocolate", false, false, "next year", 100);
+
+        System.out.println("Product: "+ apple.name);
+        System.out.println("Fruit? " + apple.fruit);
+        System.out.println("Expiry date: " + apple.expiryDate);
+        System.out.println("Weight: " + apple.weight + 'g');
+
+        System.out.println("Product: "+ cabbage.name);
+        System.out.println("Vegatable? " + cabbage.vegetable);
+        System.out.println("Expiry date: " + cabbage.expiryDate);
+        System.out.println("Weight: " + cabbage.weight + 'g');
+
+        System.out.println("Product: "+ chocolate.name);
+        System.out.println("Fruit? " + chocolate.fruit);
+        System.out.println("Vegatable? " + cabbage.vegetable);
+        System.out.println("Expiry date: " + chocolate.expiryDate);
+        System.out.println("Weight: " + chocolate.weight + 'g');
 
 
     }
--- src/SecondShelf.java
+++ src/SecondShelf.java
@@ -1,30 +1,15 @@
 public class SecondShelf {
-    //Stwórz drugą klasę, zdefiniuj w niej metodę main i utwórz dwa obiekty utworzonego wcześniej typu
     public static void main(String[] args) {
-        Fridge cucumber = new Fridge();
-        cucumber.name = "Cucumber";
-        cucumber.vegetable = true;
-        cucumber.expiryDate = "next Friday";
-        cucumber.weight = 200;
+        Fridge cucumber = new Fridge("Cucumber", true,  false, "next Friday", 200);
+        Fridge mango = new Fridge("Mango", false, true, "apparently yesterday", 250);
 
-        Fridge mango = new Fridge();
-        mango.name = "Mango";
-        mango.fruit = true;
-        mango.expiryDate = "apparently yesterday";
-        mango.weight = 250;
-
-        System.out.println("Product: " + cucumber.name);
-        System.out.println("Vegatable?: " + cucumber.vegetable);
+        System.out.println("Product: "+ cucumber.name);
+        System.out.println("Vegetable? " + cucumber.vegetable);
         System.out.println("Expiry date: " + cucumber.expiryDate);
         System.out.println("Weight: " + cucumber.weight + 'g');
 
-        System.out.println("Product: " + cucumber.name);
-        System.out.println("Vegatable?: " + cucumber.vegetable);
-        System.out.println("Expiry date: " + cucumber.expiryDate);
-        System.out.println("Weight: " + cucumber.weight + 'g');
-
-        System.out.println("Product: " + mango.name);
-        System.out.println("Vegatable?: " + mango.vegetable);
+        System.out.println("Product: "+ mango.name);
+        System.out.println("Fruit? " + mango.fruit);
         System.out.println("Expiry date: " + mango.expiryDate);
         System.out.println("Weight: " + mango.weight + 'g');
     }
